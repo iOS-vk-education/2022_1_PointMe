2fd098be0f166357b683cb4df040a665fa3bd646
ptrk09 <paw.toporkov@gmail.com>
2022-03-20 22:45:11 +0300

    delete unused code

--- PointMe/ViewController.swift
+++ PointMe/ViewController.swift
@@ -4,29 +4,8 @@ import YandexMapsMobile
 
 class ViewController: UIViewController {
     
-    private let mapView: YMKMapView = YMKMapView()
-
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        initMap()
     }
-    
-    
-    private func initMap() {
-        mapView.frame = view.bounds
-        view.addSubview(mapView)
-        
-        mapView.mapWindow.map.move(
-            with: YMKCameraPosition(
-                target: YMKPoint(latitude: 53.5, longitude: 33.0),
-                zoom: 15,
-                azimuth: 0,
-                tilt: 0
-            ),
-            animationType: YMKAnimation(type: YMKAnimationType.smooth, duration: 5),
-            cameraCallback: nil
-        )
-    }
+
 }
