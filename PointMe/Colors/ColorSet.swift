d7a129e91531e49c9f74cad3187af5d2b7b886d5
ptrk09 <paw.toporkov@gmail.com>
2022-03-25 20:37:52 +0300

    add new colors

--- PointMe/Colors/ColorSet.swift
+++ PointMe/Colors/ColorSet.swift
@@ -2,4 +2,36 @@ import UIKit
 
 extension UIColor {
     static let defaultBackgroundColor: UIColor  = .init(red: 229 / 255, green: 229 / 255, blue: 229 / 255, alpha: 1)
+    
+    
+    static let textFieldPlaceholderColor: UIColor = UIColor(
+        red: 131 / 255,
+        green: 131 / 255,
+        blue: 131 / 255,
+        alpha: 1.0
+    )
+    
+    
+    static let textFieldInputColor: UIColor = UIColor(
+        red: 32 / 255,
+        green: 34 / 255,
+        blue: 37 / 255,
+        alpha: 1.0
+    )
+    
+    
+    static let authPS: UIColor = UIColor(
+        red: 60 / 255,
+        green: 60 / 255,
+        blue: 67 / 255,
+        alpha: 0.6
+    )
+    
+    
+    static let buttonColor: UIColor = UIColor(
+        red: 32 / 255,
+        green: 34 / 255,
+        blue: 37 / 255,
+        alpha: 1.0
+    )
 }
