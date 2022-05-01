0eb246a5223f143f87759a08d85735e4935f88f6
ptrk09 <paw.toporkov@gmail.com>
2022-05-01 19:19:07 +0300

    add screen for find users

--- PointMe/Find/FindVC.swift
+++
@@ -1,9 +0,0 @@
-import UIKit
-
-class FindViewController: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .defaultBackgroundColor
-        title = "Поиск"
-    }
-}
